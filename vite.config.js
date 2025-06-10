fad9111bc038896e8580639cef894d2fa7a56694
tiyanof <hannasophia1999@gmail.com>
2025-06-10 21:57:29 +0800

    updated website

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bukonatnat.github.io',
   plugins: [
-  react(),
-  tailwindcss(),
+    react(),
+    tailwindcss(),
   ],
 })
